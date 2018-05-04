0b0fac2604f722bea92fb34fded3c53a49a99631
PontusLaestadius <pontus.laestadius@gmail.com>
2018-05-04 20:58:04 +0200

    adds bodyparts layout

---
+++ ERToGo/app/src/main/java/com/er_to_go/BodyPartActivity.java
@@ -0,0 +1,18 @@
+package com.er_to_go;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class BodyPartActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bodyPart);
+    }
+
+}
--- ERToGo/app/src/main/java/com/er_to_go/MainActivity.java
+++ ERToGo/app/src/main/java/com/er_to_go/MainActivity.java
@@ -1,13 +1,39 @@
 package com.er_to_go;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+
 
 public class MainActivity extends AppCompatActivity {
+
+    Button rightLeg;
+    Intent goToBodyPart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        rightLeg = (Button)findViewById(R.id.rightLeg);
+        goToBodyPart = new Intent(this, BodyPartActivity.class);
+
+        setContentView(R.layout.self_diagnoze);
+
+        View.OnClickListener btnListener = new View.OnClickListener() {
+            @Override
+            public void onClick(android.view.View view) {
+                startActivity(goToBodyPart);
+            }
+        };
+        rightLeg.setOnClickListener(btnListener);
     }
+
 }
+
+
+
