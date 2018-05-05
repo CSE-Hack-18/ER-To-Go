d8b28214a1c741d2f850be0d05a10f353a1bc438
ninauljanic <nina.uljanic@gmail.com>
2018-05-05 16:54:05 +0200

    basic view

--- ERToGo/app/src/main/java/com/er_to_go/UserQueueStatus.java
+++ ERToGo/app/src/main/java/com/er_to_go/UserQueueStatus.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 public class UserQueueStatus extends AppCompatActivity {
 
     //data from Bundle b
-    //private String username = getIntent().getBundleExtra("EXTRA_SESSION_ID");
+    //private String username = getIntent().getBundleExtra("username");
     private int age = getIntent().getIntExtra("age", 0);
     private int intensity = getIntent().getIntExtra("intensity", 0);
     private int bodypart = getIntent().getIntExtra("bodypart", 0);
@@ -17,10 +17,26 @@ public class UserQueueStatus extends AppCompatActivity {
     private int expectedTime = getIntent().getIntExtra("expectedTime", 0);
 
 
+    String greet = "Dear " + bodypart;
+    String state = "Average waiting time based on your injury: " + 30 + "minutes";
+    String information = "You have input the following information:\nage:" + age +"\nintensity: " + intensity + "\nbodypart: " + bodypart + "\nrecurring: " + recurring;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_queue_status);
-    }
 
+        TextView greeting = findViewById(R.id.greetUser);
+        greeting.setText(greet);
+
+
+        TextView status = findViewById(R.id.userStatus);
+        status.setText(state);
+
+        TextView info = findViewById(R.id.showInput);
+        status.setText(information);
+
+
+
+    }
 }
